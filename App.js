799579615dc97a3f3c17ac006091fc814fd27837
Trisha <76239446+Trisha-Kiran@users.noreply.github.com>
2022-07-30 16:06:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import AppHeader from './components/AppHeader';
+import HomeScreen from './screens/HomeScreen';
+import Confirmation from './screens/Confirmation';
+import firebase from "firebase";
+import db from './config'
+
+export default class App extends React.Component {
+render(){
+
+return(
+<View>
+<AppHeader/>
+<AppContainer />
+
+</View>
+
+)
+
+
+}
+}
+var AppNavigator= createSwitchNavigator({
+    HomeScreen : HomeScreen ,
+    Confirmation : Confirmation
+})
+
+const AppContainer=createAppContainer(AppNavigator)
